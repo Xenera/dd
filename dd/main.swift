77a0ee4fa699ba25dc2a1be921961f7081cf08b3
Ернур Сункарбек <sunkarbek.ernur@gmail.com>
2016-02-09 12:08:32 +0600

    Initial Commit

---
+++ dd/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  dd
+//
+//  Created by Ернур Сункарбек on 09.02.16.
+//  Copyright © 2016 Ернур Сункарбек. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
